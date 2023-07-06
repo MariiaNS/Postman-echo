71d735b4ef1ef2e4c04ca04f322faf05c6b8d552
Maria <smn0101@yandex.ru>
2023-07-07 01:05:43 +0300

    Initial commit

---
+++ src/test/java/ru/netology/MainTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void shouldReturnEndData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some"));
+
+
+    }
+}
