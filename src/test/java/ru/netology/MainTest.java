cc65721737ed7125fda2acc0a691f69d1e6e9ebf
Maria <smn0101@yandex.ru>
2023-07-07 01:16:33 +0300

    BUILD SUCCESSFUL

--- src/test/java/ru/netology/MainTest.java
+++ src/test/java/ru/netology/MainTest.java
@@ -17,7 +17,7 @@ class MainTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some"));
+                .body("data", equalTo("some data"));
 
 
     }
